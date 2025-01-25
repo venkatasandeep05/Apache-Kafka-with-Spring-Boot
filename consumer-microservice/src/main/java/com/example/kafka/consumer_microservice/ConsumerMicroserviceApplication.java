5102d71243ee3757a9b48dd496e36edcfbd74fac
venkatasandeep05 <venkatasandeep05@gmail.com>
2025-01-25 11:39:17 +0530

    Added consumer microservice project template

---
+++ consumer-microservice/src/main/java/com/example/kafka/consumer_microservice/ConsumerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka.consumer_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMicroserviceApplication.class, args);
+	}
+
+}
